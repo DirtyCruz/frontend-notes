613227d39dbfc5629c24e638056029a86af64594
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-03 17:32:35 -0700

    plugged everything, updated my snapshot and I have full functionality

--- src/containers/ViewNotes.js
+++ src/containers/ViewNotes.js
@@ -1,6 +1,42 @@
 import { connect } from 'react-redux';
 import Notes from '../components/noteList/Notes';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getAllNotes, getLoading } from '../selectors/addNoteSelectors';
+import { fetchNotes } from '../actions/addNoteActions';
+
+class ViewNotes extends Component {
+  static propTypes = {
+    fetch: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired, 
+    notes: PropTypes.array.isRequired
+  }
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    const { loading, notes } = this.props;
+    if(loading) return <h1>LOADING</h1>;
+    return (
+      <Notes notes={notes} />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  notes: getAllNotes(state),
+  loading: getLoading(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    dispatch(fetchNotes());
+  }
+});
 
 export default connect(
-
-)(Notes);
+  mapStateToProps,
+  mapDispatchToProps
+)(ViewNotes);
