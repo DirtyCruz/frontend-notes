714657765f100b0bb6c5526af2df9f231435ae3f
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-03 17:04:32 -0700

    added my Notes into a container and put it in app and updated the snapshot

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CreateNote from '../containers/CreateNote';
+import ViewNotes from '../containers/ViewNotes';
 
 export default function App() {
   return (
     <>
       <CreateNote />
+      <ViewNotes />
     </>
   );
 }
---
+++ src/containers/ViewNotes.js
@@ -0,0 +1,6 @@
+import { connect } from 'react-redux';
+import Notes from '../components/noteList/Notes';
+
+export default connect(
+
+)(Notes);
