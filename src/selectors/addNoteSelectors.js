ef54157c0ce560e122a3bd2546fb47d90f169343
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-03 17:13:33 -0700

    added my selectors

--- src/selectors/addNoteSelectors.js
+++ src/selectors/addNoteSelectors.js
@@ -1,2 +1,4 @@
 export const getTitle = state => state.addNote.title;
 export const getBody = state => state.addNote.body;
+export const getAllNotes = state => state.addNote.notes;
+export const getLoading = state => state.addNote.loading;
