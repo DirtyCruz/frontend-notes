5fe90705277e73ad336bd97991340cb37fff7a58
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-03 17:11:58 -0700

    put my action creators in my reducer

--- src/reducers/addNoteReducer.js
+++ src/reducers/addNoteReducer.js
@@ -1,9 +1,10 @@
-import { UPDATE_TITLE, UPDATE_BODY, MAKE_NOTE } from '../actions/addNoteActions';
+import { UPDATE_TITLE, UPDATE_BODY, MAKE_NOTE, FETCH_NOTES, FETCH_NOTES_LOADING } from '../actions/addNoteActions';
 
 const initialState = {
   notes: [],
   title: '',
-  body: ''
+  body: '',
+  loading: false
 };
 
 export default function reducer(state = initialState, action) {
@@ -14,6 +15,10 @@ export default function reducer(state = initialState, action) {
       return { ...state, body: action.payload };
     case MAKE_NOTE:
       return { ...state, notes: [...state.notes, action.payload] };
+    case FETCH_NOTES_LOADING:
+      return { ...state, loading: true };
+    case FETCH_NOTES:
+      return { ...state, notes: action.payload };
     default:
       return state;
   }
